81b8b6f2fdb07d75f9b97e09dcff0a1200325a13
YusuffAhmad <yusuffahmad2005@gmail.com>
2024-02-08 20:33:38 +0100

    data types and variable

--- index.php
+++ index.php
@@ -1,13 +1,72 @@
 <?php
+//////////////////////////////
+// Commenting
+/////////////////////////////
+
 /*echo "Hello FUNAAB";
 
-
 $name = 'John';*/
+
 # $age = 5;
 
-$x = 5;
-$y = 4;
-// echo $x + $y;
+// echo $age;
 
-$x = 7;
-var_dump($x);
+//////////////////////////////
+// Variables
+/////////////////////////////
+// $age = 5;
+// $name = "Emmanuel";
+
+# echo "Hello, my name is " . $name . " i am " . $age . " years old";
+
+
+//////////////////////////////
+// Data Types
+/////////////////////////////
+
+//==================
+// STRING
+//==================
+
+// $x = "John";
+// echo "Hello\n$x";
+
+// echo '<br/> ';
+
+// $x = "John";
+// echo 'Hello $x';
+
+// $x = "Hello World!";
+// echo strtoupper($x);
+
+// $x = "Hello World!";
+// echo strrev($x);
+
+//==================
+// NUMBERS
+//==================
+
+// $a = 5;
+// $b = 5.34;
+// $c = "Yusuff";
+
+// var_dump($a);
+// var_dump($b);
+// var_dump($c);
+// $x = 5985;
+// var_dump(is_numeric($x));
+
+// $x = "5985";
+// var_dump(is_numeric($x));
+
+// Cast float to int
+$x = 23465.768;
+$int_cast = (int)$x;
+echo $int_cast;
+
+echo "<br>";
+
+// Cast string to int
+$x = "23465.768";
+$int_cast = (int)$x;
+echo $int_cast;
